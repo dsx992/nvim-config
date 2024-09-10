a0b566c328a5286acdb27c81a1a3783f6fe0e644
dsx992 <145356367+dsx992@users.noreply.github.com>
2024-09-10 14:57:33 +0200

    gitignore lazy-lock

--- init.lua
+++ init.lua
@@ -12,6 +12,21 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
+vim.api.nvim_create_autocmd("FileType", {
+
+        pattern = "asm",
+
+            command = "setlocal shiftwidth=8 tabstop=8"
+
+        })
+
+vim.api.nvim_create_autocmd("FileType", {
+
+        pattern = "s",
+
+            command = "setlocal shiftwidth=8 tabstop=8"
+
+        })
 -- cursor padding
 vim.opt.scrolloff = 7
 
