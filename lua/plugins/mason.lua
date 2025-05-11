9a4b5522ef8672de8c4e89182ec8a48a67449e86
Marcus Janum <dsx992@alumni.ku.dk>
2025-05-11 13:56:56 +0200

    - vimtex - smartcase

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,10 @@
 return {
     "williamboman/mason.nvim",
-    opts = {},
 
     "williamboman/mason-lspconfig.nvim",
     name = "mason-lspconfig",
     opts = {
-        ensure_installed = { "lua_ls", "csharp_ls"},
+        ensure_installed = { "lua_ls", "csharp_ls", "clangd"},
     },
 
     "neovim/nvim-lspconfig",
