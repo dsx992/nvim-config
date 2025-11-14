cf6da82cb447b69854a544ddf37cb1ee6fe50b94
Marcus Janum <dsx992@alumni.ku.dk>
2025-11-14 16:39:18 +0100

    nleh

--- init.lua
+++ init.lua
@@ -1,20 +1,3 @@
-vim.o.expandtab = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 0
-vim.o.number = true
-vim.o.scrolloff = 0
-vim.o.colorcolumn = "80"
-vim.o.showtabline=2
-vim.cmd.colorscheme "wildcharm"
-vim.o.background="light"
-vim.keymap.set("i", "jj", "<Esc>", {})
-vim.keymap.set("t", "jj", "<C-\\><C-n>", {})
-vim.cmd([[
-    hi statusline guibg=NONE
-    hi statuslineNC guibg=NONE
-    set shortmess+=I
-]])
-
 vim.pack.add({
     { src = "https://github.com/lervag/vimtex"},
     { src = "https://github.com/BeneCollyridam/futhark-vim"},
@@ -40,6 +23,7 @@ vim.lsp.enable({
     "clangd",
     "fsautocomplete",
     "futhark_lsp",
+    "pylsp",
 })
 
 vim.lsp.config("futhark_lsp", {
@@ -48,3 +32,19 @@ vim.lsp.config("futhark_lsp", {
         vim.lsp.semantic_tokens.enable(not vim.lsp.semantic_tokens.is_enabled())
     }
 })
+
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 0
+vim.o.number = true
+vim.o.colorcolumn = "80"
+vim.o.showtabline=2
+vim.cmd.colorscheme "wildcharm"
+vim.o.background="light"
+vim.cmd([[
+    hi statusline guibg=NONE
+    hi statuslineNC guibg=NONE
+    set shortmess+=I
+]])
+vim.keymap.set("i", "<c-j>", "<Esc>", {})
+vim.keymap.set("t", "<c-j>", "<C-\\><C-n>", {})
